8a59ba6f2ccfdeb9751f32f32e7c66d56a176cc3
DarkTech1205 <ffdtjy924k@privaterelay.appleid.com>
2026-07-31 13:10:09 -0400

    Refactor PoseStack translation truncation method

--- floatify-26.2/common/src/main/java/com/example/floatify/mixin/StripelandsForcerMixin.java
+++ floatify-26.2/common/src/main/java/com/example/floatify/mixin/StripelandsForcerMixin.java
@@ -1,29 +1,24 @@
 package com.example.floatify.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.llamalad7.mixinextras.injector.ModifyParam;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(PoseStack.class)
 public class StripelandsForcerMixin {
 
-    /**
-     * Truncates all three translation axes (X, Y, Z) of the PoseStack down to 
-     * 32-bit floats simultaneously, forcing the Stripelands vertex tearing at high coordinates.
-     */
-    @ModifyArgs(
-        method = "translate(DDD)V",
-        at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack$Pose;translate(DDD)Lcom/mojang/blaze3d/vertex/PoseStack$Pose;")
-    )
-    private static void floatify$truncatePoseTranslation(Args args) {
-        double x = args.get(0);
-        double y = args.get(1);
-        double z = args.get(2);
+    @ModifyParam(method = "translate(DDD)V", index = 0)
+    private double floatify$truncateTranslateX(double x) {
+        return (float) x;
+    }
 
-        args.set(0, (double) (float) x);
-        args.set(1, (double) (float) y);
-        args.set(2, (double) (float) z);
+    @ModifyParam(method = "translate(DDD)V", index = 1)
+    private double floatify$truncateTranslateY(double y) {
+        return (float) y;
+    }
+
+    @ModifyParam(method = "translate(DDD)V", index = 2)
+    private double floatify$truncateTranslateZ(double z) {
+        return (float) z;
     }
 }
